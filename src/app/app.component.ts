5a052bcf9413d063c55fc01c84b3c981f27f313c
Ali Raza <alirazame5@gmail.com>
2018-06-12 02:49:17 +0400

    sdf
    sdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,18 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { WelcomePage } from '../pages/welcome/welcome';
-<<<<<<< HEAD
-=======
-import { AccountPage } from '../pages/account/account';
 import { TutorialPage } from '../pages/tutorial/tutorial';
->>>>>>> 59d3a9db650c72c2d8a49da6b982796288c057c1
+
+
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-<<<<<<< HEAD
-  rootPage:any = WelcomePage;
-=======
+
   rootPage:any = TutorialPage;
->>>>>>> 59d3a9db650c72c2d8a49da6b982796288c057c1
+
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
