79fd7e2541d18a720e0c06d57c88c8f68bca47fa
brandmate <39992591+brandmate@users.noreply.github.com>
2018-06-12 15:08:16 +0400

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { PdpPage } from '../pages/pdp/pdp';
 import { TutorialPage } from '../pages/tutorial/tutorial';
 
 @Component({
